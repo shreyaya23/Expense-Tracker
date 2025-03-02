ec7e680e98cd35db4419ae4d0afece92bb16e670
Shreya Singh <shreyashravansingh@gmail.com>
2025-03-02 14:34:55 +0530

    Passport Configure

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,51 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 
 import { ApolloServer } from "@apollo/server"
 import { buildSchema } from "graphql"
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import { buildContext } from "graphql-passport";
+
 import mergedResolvers from "./resolvers/index.js"
 import mergedTypeDefs from "./typeDefs/index.js"
 
 import {connectDb} from "./db/connectDB.js"
+import { configurePassport } from "./passport/passportConfig.js";
 
 dotenv.config();
+configurePassport();
+
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+})
+
+store.on("error", (err) => console.log(err));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //this option specifies whether to save the session to the store on every req
+    saveUninitialized: false, //this option specifies whether to save uninitialized sessions
+    cookie:{
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true // prevents css attacks
+    },
+    store: store, 
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
  
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -29,12 +60,15 @@ await server.start();
 // and our expressMiddleware function.
 app.use(
   '/',
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req , res}) => buildContext({ req , res }),
   }),
 );
 
