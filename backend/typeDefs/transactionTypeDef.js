ba642711cf23cb92a21a2c85efb22296a9912fa1
Shreya Singh <shreyashravansingh@gmail.com>
2025-03-14 19:12:30 +0530

    CRUD added

--- backend/typeDefs/transactionTypeDef.js
+++ backend/typeDefs/transactionTypeDef.js
@@ -1,45 +1,51 @@
 const transactionTypeDef = `#graphql
-    type Transaction{
-       _id: ID!
-       userId: ID!
-       description: String!
-       paymentType: String!
-       category: String!
-       amount: Float!
-       location: String!
-       date: String!
-    }
-    
-    type Query{
-        transactions: [Transaction!]
-        transaction(transactionId: ID!): Transaction
-    }
+  type Transaction {
+    _id: ID!
+    userId: ID!
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    location: String
+    date: String!
+    user: User!
+  }
 
-    type Mutation{
-        createTransaction(input: CreateTransactionInput!): Transaction!
-        updateTransaction(input: UpdateTransactionInput!): Transaction!
-        deleteTransaction(transactionId: ID!): Transaction!
-    }
+  type Query {
+    transactions: [Transaction!]
+    transaction(transactionId:ID!): Transaction
+    categoryStatistics: [CategoryStatistics!]
+  }
 
-    input CreateTransactionInput{
-        description: String!
-        paymentType: String!
-        category: String!
-        amount: Float!
-        location: String!
-        date: String!
-    }
+  type Mutation {
+    createTransaction(input: CreateTransactionInput!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
+    deleteTransaction(transactionId:ID!): Transaction!
+  }
 
-    input UpdateTransactionInput{
-        transactionId: ID!
-        description: String!
-        paymentType: String!
-        category: String!
-        amount: Float!
-        location: String!
-        date: String!
-    }
+  type CategoryStatistics {
+    category: String!
+    totalAmount: Float!
+  }
 
-    `
+  input CreateTransactionInput {
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    date: String!
+    location: String
+  }
+
+  input UpdateTransactionInput {
+    transactionId: ID!
+    description: String
+    paymentType: String
+    category: String
+    amount: Float
+    location: String
+    date: String
+  }
+`;
 
 export default transactionTypeDef;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -11,8 +11,8 @@ const LoginPage = () => {
 		password: "",
 	});
 
-	const [login, {loading}] = useMutation(LOGIN, {
-		refetchQueries: ["GET_AUTHENTICATED_USER"],
+	const [login, { loading }] = useMutation(LOGIN, {
+		refetchQueries: ["GetAuthenticatedUser"],
 	});
 
 	const handleChange = (e) => {
@@ -63,12 +63,11 @@ const LoginPage = () => {
 							<div>
 								<button
 									type='submit'
-									className='w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300
-										disabled:opacity-50 disabled:cursor-not-allowed
+									className='w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed
 									'
 									disabled={loading}
 								>
-									{loading? "Loading..." : "Login"}
+									{loading ? "Loading..." : "Login"}
 								</button>
 							</div>
 						</form>
